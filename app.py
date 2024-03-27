16038c48aaf80505ef60ead44bbbc4698c3cd9a2
Seba Mena <smenaaliaga@gmail.com>
2024-03-27 03:23:28 -0300

    Add buffer and textarea

--- app.py
+++ app.py
@@ -25,3 +25,4 @@ if __name__ == '__main__':
         app.run(debug=True, use_reloader=False)
     finally:
         serial_com.close()  # Asegúrate de cerrar la conexión serial al terminar
+
--- serial_communicator.py
+++ serial_communicator.py
@@ -1,15 +1,21 @@
 # serial_communicator.py
 
 import serial
+import time
 
 class SerialCommunicator:
     def __init__(self, port='COM3', baudrate=9600):
         self.ser = serial.Serial(port, baudrate)
         self.ser.timeout = 2
+        self.chunk_size = 32
+        self.pause_time = 0.2
     
     def write(self, data):
         """Escribe datos en el puerto serial."""
-        self.ser.write(data.encode())
+        for i in range(0, len(data), self.chunk_size):
+            chunk = data[i:i+self.chunk_size]
+            self.ser.write(chunk.encode())
+            time.sleep(self.pause_time)
 
     def read_line(self):
         """Lee una línea desde el puerto serial y la devuelve."""
